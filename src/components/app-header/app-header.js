ce7a43ecf8cc7058342b436406ada93b5bb9a9e4
Andrii <www.andriy12@gmail.com>
2022-01-12 23:21:20 +0200

    created a structure and added likes and stars for important posts

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './app-header.css';
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex" >
+            <h1>Mazur Andrii</h1>
+            <h2>0 постів , з них сподобалось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
